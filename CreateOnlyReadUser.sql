4f489d7f27579f20e50c464e0071566dead14cf0
Ivan Gonzalez <Ivan.Gonzalez@endava.com>
2019-02-07 15:44:49 -0500

    Mod to usernames for only read and read/write

--- CreateOnlyReadUser.sql
+++ CreateOnlyReadUser.sql
@@ -1,3 +1,3 @@
-CREATE USER 'ChallengeOnlyRead'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com' IDENTIFIED BY '1234567890';
-GRANT SELECT ON challenge TO 'ChallengeOnlyRead'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com';
+CREATE USER 'ChallengeOR'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com' IDENTIFIED BY '1234567890';
+GRANT SELECT ON challenge TO 'ChallengeOR'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com';
 FLUSH PRIVILEGES;
--- CreateReadWriteUser.sql
+++ CreateReadWriteUser.sql
@@ -1,5 +1,5 @@
-CREATE USER 'ChallengeReadWrite'@'mysqlchallenge.cm6hb8mfglop.us-east-1.rds.amazonaws.com' IDENTIFIED BY '1234567890';
-GRANT SELECT ON challenge TO 'ChallengeReadWrite'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com';
-GRANT UPDATE ON challenge TO 'ChallengeReadWrite'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com';
-GRANT INSERT ON challenge TO 'ChallengeReadWrite'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com';
+CREATE USER 'ChallengeRW'@'mysqlchallenge.cm6hb8mfglop.us-east-1.rds.amazonaws.com' IDENTIFIED BY '1234567890';
+GRANT SELECT ON challenge TO 'ChallengeRW'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com';
+GRANT UPDATE ON challenge TO 'ChallengeRW'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com';
+GRANT INSERT ON challenge TO 'ChallengeRW'@'mysqlchallange.cm6hb8mfglop.us-east-1.rds.amazonaws.com';
 FLUSH PRIVILEGES;
