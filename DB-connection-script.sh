dd5d65c2021d1d7f4f2be5c24836a0b5e08c0e8b
Jorge Dominguez <jorge.dominguez@endava.com>
2019-02-15 16:31:14 -0500

    Added shell script to connect to DB

---
+++ DB-connection-script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+	mysql -h mysqlchallenge.cm6hb8mfglop.us-east-1.rds.amazonaws.com -u Team2 -pEndava12345! 
+	USE challenge
+
+	SET @mean = 0;
+	SET @median = 0;
+	SET @md = 0;
+	SET @min = 0;
+	SET @max = 0;
+
+	CALL calculus(@mean, @median, @md, @min, @max);
+
+	SELECT @mean, @median, @md, @min, @max; 
+
+
+: '
+for i in {1..99}
+do
+	mysql -h mysqlchallenge.cm6hb8mfglop.us-east-1.rds.amazonaws.com -u Team2 -pEndava12345! 
+	USE challenge
+
+	SET @mean = 0;
+	SET @median = 0;
+	SET @md = 0;
+	SET @min = 0;
+	SET @max = 0;
+
+	CALL calculus(@mean, @median, @md, @min, @max);
+
+	SELECT @mean, @median, @md, @min, @max; 
+
+	if [ $((i%2)) -ne 0 ]
+	then
+    	echo $i
+	fi
+done '
