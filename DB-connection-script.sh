272c43d71cb7016a8f5de5a58b900c1048baeaac
Cristian <cristian.cristancho@endava.com>
2019-02-18 12:16:26 -0500

    Test connection and execute commands mysql

--- DB-connection-script.sh
+++ DB-connection-script.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
 
-	mysql -h mysqlchallenge.cm6hb8mfglop.us-east-1.rds.amazonaws.com -u Team2 -pEndava12345! 
-	USE challenge
-
+mysql -h localhost -u root -p1234 --execute "
+	USE db_challenge;
 	SET @mean = 0;
 	SET @median = 0;
 	SET @md = 0;
 	SET @min = 0;
 	SET @max = 0;
-
 	CALL calculus(@mean, @median, @md, @min, @max);
-
-	SELECT @mean, @median, @md, @min, @max; 
-
+	SELECT @mean, @median, @md, @min, @max;" 
 
 : '
+	mysql -h mysqlchallenge.cm6hb8mfglop.us-east-1.rds.amazonaws.com -u Team2 -pEndava12345! 
+	--execute "
+	USE db_challenge;
+	SET @mean = 0;
+	SET @median = 0;
+	SET @md = 0;
+	SET @min = 0;
+	SET @max = 0;
+	CALL calculus(@mean, @median, @md, @min, @max);
+	SELECT @mean, @median, @md, @min, @max; 
+	"
+
 for i in {1..99}
 do
 	mysql -h mysqlchallenge.cm6hb8mfglop.us-east-1.rds.amazonaws.com -u Team2 -pEndava12345! 
