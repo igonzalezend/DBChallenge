f6b94364a950a9bb33cef1c135d0f57c36dd7c91
Cristian <cristian.cristancho@endava.com>
2019-02-18 09:37:22 -0500

    Scripts DB-connection and Create Table are updated

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,12 +1,14 @@
+CREATE DATABASE db_challenge;
+
 CREATE TABLE `challenge` (
   `seq` BIGINT NULL,
   `yn` VARCHAR(1) NULL,
   `age` INT(3) NULL,
-  `birthday` VARCHAR(20) NULL,
+  `birthday` DATE NULL,
   `bool` VARCHAR(6) NULL,
   `city` VARCHAR(45) NULL,
   `ccnumber` BIGINT NULL,
-  `date` VARCHAR(20) NULL,
+  `date_c` DATE NULL,
   `digit` BIGINT NULL,
   `dollar` VARCHAR(45) NULL,
   `first` VARCHAR(45) NULL,
